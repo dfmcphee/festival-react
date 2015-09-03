d0902315cd830138617c7f365eb2bad1f04ef95f
Dominic McPhee <dominic.mcphee@gmail.com>
2015-09-02 22:17:01 -0400

    Adding comments and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Festival
+A simple location sharing app
+
+## Getting started
+
+- Install (node.js)[https://nodejs.org/en/]
+- Run `npm install` from inside this directory
+- Run `npm start` to start the server
+- Visit (http://localhost:3000)[http://localhost:3000] in a browser
--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static('public'));
 
+// Initialize empty user list
 var onlineUsers = {};
 
+// Send an updated userlist to all clients
 function updateUserList() {
   io.emit('onlineUsersUpdated', {
     onlineUsers: onlineUsers
   });
 }
 
+// Event every time a user connects
 io.on('connection', function(socket){
   console.log('a user connected');
 
+  // Add connected user to list
   onlineUsers[socket.id] = {
     id: socket.id,
     name: 'Anonymous'
@@ -24,17 +27,21 @@ io.on('connection', function(socket){
 
   updateUserList();
 
+  // Event when a user identifies themselves
   socket.on('identify', function(name){
     onlineUsers[socket.id].name = name;
     updateUserList();
   });
 
+  // Event when a user disconnects
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    // Remove them from user list
     delete onlineUsers[socket.id];
   });
 });
 
+// Start server listening on port 3000
 http.listen(3000, function(){
   console.log('listening on port 3000');
 });
